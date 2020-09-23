3e1ff49b569a327c01c95613e130b64de5dd3c45
fatur14-alt <71744210+fatur14-alt@users.noreply.github.com>
2020-09-23 09:52:26 +0700

    Add files via upload

---
+++ fatur.py
@@ -0,0 +1,165 @@
+#!/bin/bash
+#Ahmad Faturrohman
+clear
+figlet "Ahmad Faturrohman"
+echo "_________________________________"
+echo "Author : Ahmad Faturrohman"
+echo "WA____ : 085283150833"
+echo "FB____ : Fatur"
+echo
+echo "Khusus Buat Bebeb Leni♡"
+echo "[ Pilih Mau Ke Nomor Berapa ]"
+echo "[1] Puisi"
+echo "[2] Isi hati"
+echo "[3] Kisah"
+echo "[4] Bonus"
+echo "[0] Exit"
+echo
+read -p "[ Pilih Ya Nomor Nya ]》》 " pill
+#batas beib
+if [ $pill = "1" ]
+then
+echo "Sabar yaaa ....." ;sleep 2 
+
+echo "Tahun Pun Telah Kita lalui" ; sleep 2
+echo
+echo "Aku Yg tak pernah merasa rapuh" ;sleep 2
+echo
+echo "Meski Ribuan Beban Menghantam" ;sleep 2
+echo
+echo "keyakinku akan dirimu" ;sleep 2
+echo
+echo "selalu terpatri kokoh di dalam hati" ;sleep 2
+echo
+echo "senyuman indah itu selalu menjadi" ;sleep 2
+echo
+echo "pengobat segala rindu" ;sleep 2
+echo
+echo "cinta yg tulis suci ini" ;sleep 2
+echo
+echo "Jangan sampai ternoda oleh dusta" ;sleep 2
+echo
+echo "Kaulah Pelita hati dalam jiwaku" ;sleep 2
+echo
+echo "penerang setiap langkah kakiku" ;sleep 2
+echo
+echo "jangan lau engkau ragu" ;sleep 2
+echo
+echo "Aku tak akan pernah sedikit pun" ;sleep 2
+echo
+echo "Menjauh dari dirimu" ;sleep 2
+echo
+echo "Karena kaulah yg selalu ada untuk" ;sleep 2
+echo
+echo "Mengisi Rongga hidup ku" ;sleep 2
+echo
+echo
+echo "AKU SAYANG KAMU LENI ZHEHAN AFRYANI" ;sleep 3
+fi
+#batas beib
+if [ $pill = "2" ]
+then
+echo "Jujur ini baru yg nama nya cinta" ;sleep 2
+echo
+echo "Baru kali ini aku bisa seperti ini" ;sleep 2
+echo
+echo "Sebelum nya aku gapernah kaya gini" ;sleep 2
+echo
+echo "Cuman Kamu yg bisa ngerubah aku" ;sleep 2
+echo
+echo "Dan Aku juga baru kali ini bisa syg sama wanita" ;sleep 2
+echo
+echo "Mkasih ya Leni Sayang" ;sleep 2
+echo
+echo
+echo "Aku sayang kamu" ;sleep 2
+fi 
+#batas beib
+if [ $pill = "3" ]
+then
+
+echo "Berawal dari Chat di fb yg ga sengaja" ;sleep 2
+echo 
+echo "Waktu demi waktu aku mulai penasaran" ;sleep 2
+echo
+echo "dan aku coba mulai membuka hati buat kamu" ;sleep 2
+echo
+echo "aku yakin kamu pasti wanita terbaik" ;sleep 2
+echo
+echo "dan aku mulai perjuangin kamu walaupun" ;sleep 2
+echo
+echo "kamu gapernah mau ngehargain atau anggap aku ada" ;sleep 2
+echo
+echo "tapi aku ga pernah putus asa atau harapan" ;sleep 2
+echo
+echo "Untuk mmendapatkan kamu" ;sleep 2
+echo
+echo "dan waktu terus berlalu" ;sleep 2
+echo
+echo "pagi siang sore malam aku selalu di anggap tidak" ;sleep 2
+echo
+echo "ada dan aku selalu di perlakukan" ;sleep 2
+echo
+echo "seperti aku mencuri" ;sleep 2
+echo
+echo "walaupun seperti itu aku tetep berjuang" ;sleep 2
+echo
+echo "walaupun pernah ada pikiran untuk mundur" ;sleep 2
+echo
+echo "dan aku lawan pikiran itu sampe aku" ;sleep 2
+echo
+echo "bener bener putus asa dan mencari wanita lain" ;sleep 2
+echo
+echo "tapi itu ga semulus yg aku bayangkan" ;sleep 2
+echo
+echo "hati pikiran aku masih ada kamu" ;sleep 2
+echo
+echo "tidak bisa melepaskan kamu" ;sleep 2
+echo
+echo "aku mencoba mendapatkan kamu lagi" ;sleep 2
+echo
+echo "walaupun pernah jatuh lagi akibat suatu hal" ;sleep 2
+echo
+echo "dan aku mulai mencoba dan sampe kamu mau nerima" ;sleep 2
+echo
+echo "aku sampe sekarang ini" ;sleep 2
+echo
+echo
+echo "AKU SAYANG KAMU LENI ZHEHAN AFRYANI"
+fi
+#batas beib
+if [ $pill = "4" ]
+then
+echo "Duh apa yaa yg terakhir" ;sleep 4
+echo
+echo "aku bingung" ;sleep 4
+echo
+echo "udah gaada kata kata lagi sih buat bahagia nya" ;sleep 4
+echo
+echo "aku dapetin kamu" ;sleep 4
+echo
+echo
+echo "Jangan Pernah Berubah ya sayang"
+fi
+#batas beib
+if [ $pill = "0" ]
+then
+echo "A" ;sleep 2
+echo "K" ;sleep 2
+echo "U" ;sleep 2
+echo
+echo "S" ;sleep 2
+echo "A" ;sleep 2
+echo "Y" ;sleep 2
+echo "A" ;sleep 2
+echo "N" ;sleep 2
+echo "G" ;sleep 2
+echo
+echo "K" ;sleep 2
+echo "A" ;sleep 2
+echo "M" ;sleep 2
+echo "U" ;sleep 4
+
+exit
+fi
+
